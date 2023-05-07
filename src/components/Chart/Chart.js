cf5626902669566e287576bb40b33639d39daee5
Yash1Hingu <yash23hingu@gmail.com>
2023-05-07 08:12:32 +0530

    create JSX structure for Chart component

--- src/components/Chart/Chart.js
+++ src/components/Chart/Chart.js
@@ -1,7 +1,16 @@
 import ChartBar from'./ChartBar'
 import './Chart.css'
 const Chart = props => {
-
+    return (
+        <div>
+            {props.dataPoints.map(dataPoint => <ChartBar 
+                key={dataPoint.label}
+                value={dataPoint.value}
+                maxValue={null}
+                label={dataPoint.label}
+            />)}
+        </div>
+    )
 }
 
 export default Chart;
